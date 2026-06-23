35db2cd9768296d86c68962ea26aec67a1e50267
UWIMPUHWE Regis <santanadox349@gmail.com>
2026-06-23 14:42:47 +0200

    Add .nojekyll to fix GitHub Pages ignoring _next/ folder

--- install-gh.ps1
+++
@@ -1,7 +0,0 @@
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Write-Host "Downloading GitHub CLI..."
-Invoke-WebRequest -Uri "https://github.com/cli/cli/releases/download/v2.95.0/gh_2.95.0_windows_amd64.msi" -OutFile "$env:TEMP\gh.msi" -UseBasicParsing
-Write-Host "Installing..."
-Start-Process msiexec.exe -ArgumentList "/i `"$env:TEMP\gh.msi`" /quiet /norestart" -Wait
-Remove-Item "$env:TEMP\gh.msi"
-Write-Host "Done"
